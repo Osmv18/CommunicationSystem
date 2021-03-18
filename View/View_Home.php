4ee81ed5a1d5d31b443ce2bb015e290a36975b87
Osmv18 <osmedranov10@yahoo.es>
2021-03-17 22:02:16 -0600

    Se mejoró el home - carousel

--- View/View_Home.php
+++ View/View_Home.php
@@ -8,22 +8,22 @@
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="Images/Fondo_Carrousel_1.png" class="d-block w-100" alt="...">
-        <div class="centrado">Escuela de Ingeniería de Sistemas</div>       
-        <div class="padre">
-          <span class="hijo">Te damos la bienvenida a este portal 
-            en la cual podrás tener un mayor contacto con los servicios
-            que se te provee. De estudiantes para estudiantes.</span>
-        </div>
+          <div class="centrado">Escuela de Ingeniería de Sistemas</div>       
+          <div class="padre">
+            <span class="hijo">Te damos la bienvenida a este portal 
+              en la cual podrás tener un mayor contacto con los servicios
+              que se te provee. De estudiantes para estudiantes.</span>
+          </div>
 
       </div>
       <div class="carousel-item">
         <img src="Images/Fondo_Carrousel_2.png" class="d-block w-100" alt="...">
-        <div class="centrado">Escuela de Ingeniería de Sistemas</div>       
-        <div class="padre">
-          <span class="hijo">Te damos la bienvenida a este portal 
-            en la cual podrás tener un mayor contacto con los servicios
-            que se te provee. De estudiantes para estudiantes.</span>
-        </div>
+          <div class="centrado">Escuela de Ingeniería de Sistemas</div>       
+          <div class="padre">
+            <span class="hijo">Te damos la bienvenida a este portal 
+              en la cual podrás tener un mayor contacto con los servicios
+              que se te provee. De estudiantes para estudiantes.</span>
+          </div>
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
@@ -37,6 +37,24 @@
   </div>
 </div>
 
-<div class="container-fluid">
-  
+<div class="container-fluid" id="contacts">
+  <div class="card" style="width: 18rem;">
+    <div class="card-header">
+      Contactos
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Fabiola Chavarría Arredondo.<br>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+          </svg>  fabiola.chavarria@ulatina.cr</li>
+      <li class="list-group-item">Jose Remón. <br>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+          </svg>  jose.remon@ulatina.cr</li>
+      <li class="list-group-item">Carlos Zúñiga.<br>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+          </svg>  carlos.zuniga@ulatina.cr</li>
+    </ul>
+  </div>
 </div>
