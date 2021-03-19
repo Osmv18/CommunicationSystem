e3dcddbc04a35a810e450414046d8c5f1f0dff92
dancor98 <dancor98@MacBook-Pro.local>
2021-03-18 20:05:57 -0600

    prueba

--- View/Header.php
+++ View/Header.php
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto">
 	<li class="nav-item active">
-	  <a class="nav-link" href="index.php" >Bienvenida<span class="sr-only"></span></a>
+	  <a class="nav-link" href="index.php" >Bienvenda<span class="sr-only"></span></a>
 	</li>
 	<li class="nav-item active">
 	  <a class="nav-link" href="index.php" >Iniciar sesión<span class="sr-only"></span></a>
