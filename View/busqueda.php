8a0e2c307cb95eef7748d90a8af0788caa2d5502
dancor98 <dancor98@MacBook-Pro.local>
2021-03-20 10:41:29 -0600

    vistas daniel

---
+++ View/busqueda.php
@@ -0,0 +1,15 @@
+<div id="nueva_publicacion">
+    <a href="#" class="btn_nueva_publicacion">Nueva Publicación</a> 
+</div>
+<br>
+<div id="buscar">
+    <br>
+    <h2>Filtro de busqueda</h2>
+    <hr>
+    <a href="#" class="btn_busqueda">Empleos</a>
+    <br><br>
+    <a href="#" class="btn_busqueda">PES</a> 
+    <br><br>
+    <a href="#" class="btn_busqueda">TCU</a> 
+    <br><br>
+</div>
---
+++ View/menu2.php
@@ -0,0 +1,29 @@
+
+    <nav class="nav">
+        <a class="logo" href="../publicaciones.php"><img src="../img/logo.png" alt="Logo Escuela Ingenieria en Sistemas"></a>
+        
+        <button class="nav-toggle" aria-label="Abrir menú">
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <ul class="nav-menu">
+            <li class="nav-menu-item">
+                <a href="../index.php" class="nav-menu-link nav-link">Inicio</a>
+            </li>
+            <li class="nav-menu-item dropdown">
+                <a href="#" class="nav-menu-link nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Categorias
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdowm">
+                    <li><a class="dropdown-item" href="">Empleos</a></li>
+                    <li><a class="dropdown-item" href="">TCU</a></li>
+                    <li><a class="dropdown-item" href="">PES</a></li>
+                </ul>
+            </li>
+            <li class="nav-menu-item">
+                <a href="/admin_cuenta.php" class="nav-menu-link nav-link">Cuenta</a>
+            </li>
+        </ul>
+    </nav>
+
+
