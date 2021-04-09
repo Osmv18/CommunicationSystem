eae512bdfd08e8882c8d339c7bcd5ffbe12ebb4c
Osmv18 <osmedranov10@yahoo.es>
2021-04-08 21:12:03 -0600

    Arreglo database

--- Model/Config.php
+++ Model/Config.php
@@ -1,12 +1,10 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 
 define("DB_NAME", "communication_system");
 
--- Model/connection.php
+++ Model/connection.php
@@ -1,22 +1,22 @@
 <?php
-require_once 'Model/Config.php';
+
+require_once 'Model/config.php';
 
 class connection {
-    private $std;
 
-    public function __construct() {​​
-        $this->std ="mysql:dbname=".DB_NAME.";charset=utf8";
-    }​​
-    public function connect(){​​
-        try{
-            ​​return new PDO($this->std,DB_USER,
-            DB_PASSWORD);
-        }​​ 
-        catch (PDOException $ex){
-            ​​echo("Error en la funcion ". __FUNCTION__." del archivo ".__FILE__." el error es: ".
-            $ex->getMessage());
-        }​​
-        return null;
-    }​​
+  private $std;
+
+  public function __construct() {
+    $this->std = "mysql:dbname=" . DB_NAME . ";charset=utf8";
+  }
+
+  public function connect() {
+    try {
+      return new PDO($this->std, DB_USER, DB_PASSWORD);
+    } catch (PDOException $ex) {
+      echo("Error en la funcion " . __FUNCTION__ . " del archivo " . __FILE__ . " el error es: " . $ex->getMessage());
+    }
+    return null;
+  }
 
 }
